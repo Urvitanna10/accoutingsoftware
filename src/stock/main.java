0bd747a7f29d78a65e05b0be38c8629e44572ccf
Khushi-24 <khushi.jiyani1@gmail.com>
2021-02-26 22:24:49 +0530

    main.java modiefied for purchase form

--- src/stock/main.java
+++ src/stock/main.java
@@ -143,6 +143,8 @@ public class main extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        purchase p = new purchase();
+        p.setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
